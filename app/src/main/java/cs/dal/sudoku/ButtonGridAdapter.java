3899c16f57cb4e8633ff2adfa3af1d9681890913
Duncan Pulsifer <duncanpulsifer@gmail.com>
2017-02-09 18:34:38 -0400

    Main activity created
    Sudoku gridview and button gridview with hardcoded data.

---
+++ app/src/main/java/cs/dal/sudoku/ButtonGridAdapter.java
@@ -0,0 +1,45 @@
+package cs.dal.sudoku;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
+
+/**
+ * Created by duncanpulsifer on 2017-02-09.
+ */
+
+public class ButtonGridAdapter extends BaseAdapter{
+
+    private Context context;
+    private String[] buttons;
+    LayoutInflater inflater;
+
+    public ButtonGridAdapter(Context context, String[] buttons) {
+        this.context = context;
+        this.buttons = buttons;
+        inflater = (LayoutInflater) this.context.getSystemService(LAYOUT_INFLATER_SERVICE);
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if(convertView ==null) {
+            convertView = inflater.inflate(R.layout.button_cell, null);
+        }
+        Button b = (Button) convertView.findViewById(R.id.button_item);
+        b.setText(buttons[position]);
+        return convertView;
+
+    }
+
+    @Override
+    public int getCount() { return buttons.length; }
+
+    @Override public Object getItem(int position) { return buttons[position]; }
+
+    @Override public long getItemId(int position) { return position; }
+}
---
+++ app/src/main/java/cs/dal/sudoku/SudokuGridAdapter.java
@@ -0,0 +1,44 @@
+package cs.dal.sudoku;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
+
+/**
+ * Created by duncanpulsifer on 2017-02-01.
+ */
+
+public class SudokuGridAdapter extends BaseAdapter
+{
+    private Context context;
+    private String[] items;
+    LayoutInflater inflater;
+
+    public SudokuGridAdapter(Context context, String[] items) {
+        this.context = context;
+        this.items = items;
+        inflater = (LayoutInflater) this.context.getSystemService(LAYOUT_INFLATER_SERVICE);
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if(convertView ==null) {
+            convertView = inflater.inflate(R.layout.sudoku_cell, null);
+        }
+        TextView t = (TextView) convertView.findViewById(R.id.grid_item);
+        t.setText(items[position]);
+        return convertView;
+    }
+
+    @Override
+    public int getCount() { return items.length; }
+
+    @Override public Object getItem(int position) { return items[position]; }
+
+    @Override public long getItemId(int position) { return position; }
+}
