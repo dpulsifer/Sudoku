30e67b469d95b4d258dfb38deb2ac533671e6388
Duncan Pulsifer <duncanpulsifer@gmail.com>
2017-02-11 11:09:54 -0400

    Implemented solved sudoku grid generator and grid digger

--- app/src/main/java/cs/dal/sudoku/MainActivity.java
+++ app/src/main/java/cs/dal/sudoku/MainActivity.java
@@ -2,7 +2,6 @@ package cs.dal.sudoku;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -10,10 +9,9 @@ import android.widget.GridView;
 public class MainActivity extends AppCompatActivity {
 
     public GridView gridView;
-    public String[] items = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28",
-            "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50",
-            "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75",
-            "76", "77", "78", "79", "80", "81"};
+    public String[] solvedSudokuGrid;
+    public String[] dugSudokuGrid;
+    public String[] referenceGrid;
 
     public GridView buttonGridView;
     public String[] buttons  = new String[] {"1","2","3","4","5","6","7","8","9","C"};
@@ -26,8 +24,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        solvedSudokuGrid = SolvedSudokuGenerator.generatePuzzle();
+        dugSudokuGrid = SolvedSudokuDigger.digPuzzle(solvedSudokuGrid, 0);
+        referenceGrid = dugSudokuGrid.clone();
+
+
         gridView = (GridView)this.findViewById(R.id.myGridView);
-        SudokuGridAdapter gridAdapter = new SudokuGridAdapter(MainActivity.this, items);
+        SudokuGridAdapter gridAdapter = new SudokuGridAdapter(MainActivity.this, dugSudokuGrid);
         gridView.setAdapter(gridAdapter);
 
         buttonGridView = (GridView)this.findViewById(R.id.buttonGridView);
@@ -50,9 +53,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
 
-                items[position] = number;
-                SudokuGridAdapter gridAdapter = new SudokuGridAdapter(MainActivity.this, items);
-                gridView.setAdapter(gridAdapter);
+                if (referenceGrid[position].equals(" ")) {
+                    dugSudokuGrid[position] = number;
+                    SudokuGridAdapter gridAdapter = new SudokuGridAdapter(MainActivity.this, dugSudokuGrid);
+                    gridView.setAdapter(gridAdapter);
+                }
 
             }
         });
---
+++ app/src/main/java/cs/dal/sudoku/SolvedSudokuDigger.java
@@ -0,0 +1,122 @@
+package cs.dal.sudoku;
+
+/**
+ * Created by duncanpulsifer on 2017-02-11.
+ */
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class SolvedSudokuDigger {
+
+    private static ArrayList<Integer> gridCoordinatesRemoved = new ArrayList<Integer>(81);
+    private static ArrayList<Integer> gridCoordinatesChecked = new ArrayList<Integer>(81);
+
+    private static int[] totalElementsForDiffLevel = new int[]{45, 35, 30, 25};
+    private static int[] lowerBoundForDiffLevel = new int[]{4,3,2,0};
+
+    private static Random random = new Random();
+
+    public static String[] digPuzzle(String[] sudokuGrid, int difficultyLevel) {
+
+
+        for (int j = 0; j < 81; j++) {
+            gridCoordinatesRemoved.add(j, j);
+            gridCoordinatesChecked.add(j, j);
+        }
+
+        int[][] puzzle = stringToIntGrid(sudokuGrid);
+        int gridElement;
+        int xCoord;
+        int yCoord;
+
+        while (gridCoordinatesRemoved.size() >= totalElementsForDiffLevel[difficultyLevel]) {
+
+            gridElement = random.nextInt(gridCoordinatesChecked.size());
+            xCoord = gridCoordinatesChecked.get(gridElement) % 9;
+            yCoord = gridCoordinatesChecked.get(gridElement) / 9;
+
+            if (checkRow(puzzle, xCoord, lowerBoundForDiffLevel[difficultyLevel]) &&
+                    checkColumn(puzzle, yCoord, lowerBoundForDiffLevel[difficultyLevel]) &&
+                    checkBox(puzzle, xCoord, yCoord, lowerBoundForDiffLevel[difficultyLevel])) {
+
+                puzzle[xCoord][yCoord] = -1;
+
+                gridCoordinatesRemoved.remove(gridElement);
+            }
+
+            gridCoordinatesChecked.remove(gridElement);
+
+
+        }
+
+        String[] puzzleIntToString = new String[81];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (puzzle[i][j] == -1) {puzzleIntToString[(i*9) + j] = " ";}
+                else {puzzleIntToString[(i*9) + j] = Integer.toString(puzzle[i][j]);}
+            }
+        }
+
+        return puzzleIntToString;
+
+
+    }
+
+    private static boolean checkRow(int[][] sudokuGrid, int xCoord, int lowerBoundForDiffLevel) {
+
+        int count = -1;
+
+        for (int i = 0; i < 9; i++) {
+            if (sudokuGrid[xCoord][i] != -1) { count++; }
+        }
+
+        return (count >= lowerBoundForDiffLevel);
+
+    }
+
+    private static boolean checkColumn(int[][] sudokuGrid, int yCoord, int lowerBoundForDiffLevel) {
+
+        int count = -1;
+
+        for (int i = 0; i < 9; i++) {
+            if (sudokuGrid[i][yCoord] != -1) { count++; }
+        }
+
+        return (count >= lowerBoundForDiffLevel);
+
+    }
+
+    private static boolean checkBox(int[][] sudokuGrid, int xCoord, int yCoord, int lowerBoundForDiffLevel) {
+
+        int startingXCoord = (xCoord / 3) * 3;
+        int startingYCoord = (yCoord / 3) * 3;
+
+        int count = -1;
+
+        for (int i = startingXCoord; i < startingXCoord + 3; i++) {
+            for (int j = startingYCoord; j < startingYCoord + 3; j++) {
+                if (sudokuGrid[i][j] != -1) { count++; }
+            }
+        }
+
+        return (count >= lowerBoundForDiffLevel);
+
+    }
+
+    private static int[][] stringToIntGrid(String[] solvedSudokuGrid) {
+
+        int[][] puzzle = new int[9][9];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                puzzle[i][j] = Integer.parseInt(solvedSudokuGrid[(i*9) + j]);
+            }
+        }
+
+        return puzzle;
+
+    }
+
+}
